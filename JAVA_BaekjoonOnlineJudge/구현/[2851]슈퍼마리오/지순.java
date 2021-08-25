74424df193b14a3a865b60c92fd190866c04e6de
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-08-25 23:58:40 +0900

    Create 지순.java

---
+++ JAVA_BaekjoonOnlineJudge/구현/[2851]슈퍼마리오/지순.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (sum + num < 100)
+				sum += num;
+			else {
+				if (Math.abs(sum + num - 100) < Math.abs(sum - 100)) {
+					sum += num;
+					break;
+				} else if (Math.abs(sum + num - 100) == Math.abs(sum - 100)) {
+					sum += num;
+					break;
+				} else
+					break;
+			}
+		}
+		System.out.println(sum);
+	}
+}
