7927f40dd348451da26b5cdf200753a69294317c
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-11-01 19:46:27 +0900

    Update 지순.java

--- JAVA_BaekjoonOnlineJudge/삼성 SW 역량 테스트 기출 문제/[14890]경사로/지순.java
+++ JAVA_BaekjoonOnlineJudge/삼성 SW 역량 테스트 기출 문제/[14890]경사로/지순.java
@@ -32,22 +32,19 @@ public class BJ_경사로_14890 {
 	}
 	
 	private static void solve(int[][] temp) {
-		for(int i=0;i<N;i++) {
+		outer: for(int i=0;i<N;i++) {
 			int height = temp[i][0];
 			int cnt = 1;
-			boolean check = false;
 			for(int j=1;j<N;j++) {
 				if(Math.abs(temp[i][j]-height)>1) {
-					check = true;
-					continue;
+					continue outer;
 				}
 				if(temp[i][j] == height) {
 					cnt++;
 				} else {
 					if(temp[i][j] < height) { // 내리막
 						if(j+L>N || cnt+L>N) {
-							check = true;
-							break;
+							continue outer;
 						} else {
 							int count = 1;
 							for(int k=j+1;k<j+L;k++) {
@@ -60,8 +57,7 @@ public class BJ_경사로_14890 {
 								height = temp[i][j];
 								j += count-1;
 							} else {
-								check = true;
-								break;
+								continue outer;
 							}
 						}
 					} else { // 오르막
@@ -69,15 +65,12 @@ public class BJ_경사로_14890 {
 							cnt = 1;
 							height = temp[i][j];
 						} else {
-							check = true;
-							break;
+							continue outer;
 						}
 					}
 				}
 			}
-			if(!check) {
-				result++;
-			}
+			result++;
 		}
 	}
 }
