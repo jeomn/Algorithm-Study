d51870cbc41ef05e78258124adb22aa102b8f5dc
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2021-12-23 18:14:35 +0900

    Create 지순.java

---
+++ JAVA_Programmers/카카오 기출문제/Level 1/크레인 인형뽑기 게임/지순.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int move: moves) {
+            for(int i=0;i<board.length;i++) {
+                if(board[i][move-1] != 0) {
+                    if(!stack.isEmpty() && stack.peek() == board[i][move-1]) {
+                        stack.pop();
+                        answer+=2;
+                    } else {
+                        stack.push(board[i][move-1]);
+                    }
+                    board[i][move-1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
