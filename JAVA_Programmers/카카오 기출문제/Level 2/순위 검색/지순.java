7adbf51461e05712ab991145e2576ed7cf710a09
JISOONLEE <46081043+JISOONLEE@users.noreply.github.com>
2022-01-30 14:18:46 +0900

    Create 지순.java

---
+++ JAVA_Programmers/카카오 기출문제/Level 2/순위 검색/지순.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+    boolean[] visited;
+    
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
+        
+        for(int i=0;i<info.length;i++) {
+            visited = new boolean[5];
+            String[] st = info[i].split(" ");
+            comb(0, 0, Integer.parseInt(st[4]), "", st);
+        }
+        
+        for(String s : map.keySet()){
+            ArrayList<Integer> l = map.get(s);
+            Collections.sort(l);
+            map.put(s, l);
+        }
+        
+        for(int i=0;i<query.length;i++) {
+            String[] st = query[i].split(" ");
+            int num = Integer.parseInt(st[st.length - 1]);
+            st[st.length - 1] = "";
+            
+            String result = st[0] + st[2] + st[4] + st[6];
+            
+            if(map.containsKey(result)) {
+                ArrayList<Integer> list = map.get(result);
+                int start = 0;
+                int end = list.size();
+                while(start < end) {
+                    int mid = (start + end) / 2;
+                    if(list.get(mid) >= num)
+                        end = mid;
+                    else
+                        start = mid + 1;
+                }
+                answer[i] = list.size() - start;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void comb(int cnt, int idx, int num, String st, String[] str) {
+        if(cnt == 4) {
+            ArrayList<Integer> list = new ArrayList<>(); 
+            if(map.containsKey(st)) {
+                list = map.get(st);
+            }
+            list.add(num);
+            map.put(st, list);
+            return;
+        }
+        
+        for(int i=idx;i<4;i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            comb(cnt+1, i, num, st + str[i], str);
+            comb(cnt+1, i, num, st + "-", str);
+            visited[i] = false;
+        }
+        
+    }
+}
